832c6645a8d2f2d0b9050d0ddc3222cf76ebd6b8
noName7700 <adlan.shamavov@mail.ru>
2024-02-21 01:31:17 +0500

    commit1

--- index.php
+++ index.php
@@ -3,102 +3,107 @@
 // Задание 1
 echo("<h3>Задание 1</h3>");
 
-$a = 3;
-$b = -4;
+$i = 0;
+do {
+    if ($i == 0) {
+        echo $i . ' - это ноль.';
+    } elseif ($i % 2 == 0) {
+        echo $i . ' - чётное число.';
+    } else {
+        echo $i . ' - нечётное число.';
+    }
+    echo '<br>';
+    $i++;
+} while ($i <= 10);
 
-if ($a >= 0 && $b >= 0) {
-    echo $a - $b;
-} elseif ($a < 0 && $b < 0) {
-    echo $a * $b;
-} elseif (($a < 0 && $b >= 0) || ($a >= 0 && $b < 0)) {
-    echo $a + $b;
-}
 echo("<hr>");
 
 //Задание 2
 echo("<h3>Задание 2</h3>");
 
-$a = 2;
-switch ($a) {
-    case 0:
-        echo '0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15';
-        break;
-    case 1:
-        echo '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15';
-        break;
-    case 2:
-        echo '2 3 4 5 6 7 8 9 10 11 12 13 14 15';
-        break;
-    case 3:
-        echo '3 4 5 6 7 8 9 10 11 12 13 14 15';
-        break;
-    case 4:
-        echo '4 5 6 7 8 9 10 11 12 13 14 15';
-        break;
-    case 5:
-        echo '5 6 7 8 9 10 11 12 13 14 15';
-        break;
-    case 6:
-        echo '6 7 8 9 10 11 12 13 14 15';
-        break;
-    case 7:
-        echo '7 8 9 10 11 12 13 14 15';
-        break;
-    case 8:
-        echo '8 9 10 11 12 13 14 15';
-        break;
-    case 9:
-        echo '9 10 11 12 13 14 15';
-        break;
-    case 10:
-        echo '10 11 12 13 14 15';
-        break;
-    case 11:
-        echo '11 12 13 14 15';
-        break;
-    case 12:
-        echo '12 13 14 15';
-        break;
-    case 13:
-        echo '13 14 15';
-        break;
-    case 14:
-        echo '14 15';
-        break;
-    case 15:
-        echo '15';
-        break;
-    default:
-        echo 'Значение "a" не входит в промежуток [0..15]';
-        break;
+$dict = [
+    'Московская область' => [
+        'Москва',
+        'Зеленоград',
+        'Клин'
+    ],
+    'Ленинградская область' => [
+        'Санкт-Петербург',
+        'Всеволожск',
+        'Павловск',
+        'Кронштадт'
+    ],
+    'Рязанская область' => [
+        'Рязань',
+        'Касимов',
+        'Ряжск',
+        'Шацк',
+        'Тума',
+    ]
+];
+foreach ($dict as $reg => $cities) {
+    echo '<h4>' . $reg . ': <br></h4>';
+    echo implode(', ', $cities) . '.';
+    echo '<br>';
 }
+
 echo("<hr>");
 
 // Задание 3
 echo("<h3>Задание 3</h3>");
 
-function add($a, $b) {
-    return $a + $b;
+$letters = array(
+    'a' => 'a',
+    'б' => 'b',
+    'в' => 'v',
+    'г' => 'g',
+    'д' => 'd',
+    'е' => 'e',
+    'ё' => 'yo',
+    'ж' => 'zh',
+    'з' => 'z',
+    'и' => 'i',
+    'й' => 'j',
+    'к' => 'k',
+    'л' => 'l',
+    'м' => 'm',
+    'н' => 'n',
+    'о' => 'o',
+    'п' => 'p',
+    'р' => 'r',
+    'с' => 's',
+    'т' => 't',
+    'у' => 'u',
+    'ф' => 'f',
+    'х' => 'x',
+    'ц' => 'c',
+    'ч' => 'ch',
+    'ш' => 'sh',
+    'щ' => 'shh',
+    'ъ' => '``',
+    'ы' => 'y',
+    'ь' => '`',
+    'э' => 'e`',
+    'ю' => 'yu',
+    'я' => 'ya'
+);
+
+$string = "раз, два, три, четыре, пять.";
+echo $string;
+echo transliteration($string);
+
+function transliteration($str) {
+    $chars = str_split($str);
+    $res = "";
+    foreach ($chars as $letter) {
+        if (array_key_exists($letter, $letters))
+            $res .= $letters[$letter];
+        else
+            $res .= $letter;
+    }
+    return $res;
 }
 
-function subtract($a, $b) {
-    return $a - $b;
-}
-
-function multiply($a, $b) {
-    return $a * $b;
-}
-
-function divide($a, $b) {
-    if ($b == 0)
-        return 'На ноль делить нельзя';
-    return $a / $b;
-}
-
-echo("<p>Сложение: " . add(3, 4) . "</p>");
-echo("<p>Вычитание: " . subtract(3, 4) . "</p>");
-echo("<p>Умножение: " . multiply(3, 4) . "</p>");
-echo("<p>Деление: " . divide(3, 4) . "</p>");
 echo("<hr>");
 
 // Задание 4
